cecb3786009eb0a0bb6c8757bc14958708e6b617
chinapicke <chinapickering@hotmail.co.uk>
2023-03-03 14:28:44 +0700

    added pages and checking navbar is being pulled into app.js

---
+++ reactapps/src/pages/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+function Header() {
+  return (
+    <h1>Hi from the navbar</h1>
+    //   <Navbar bg="dark">
+    //       <Container>
+    //         <Navbar.Brand href="#home">
+    //           <img
+    //             src="./Cp.png"
+    //             width="30"
+    //             height="30"
+    //             className="d-inline-block align-top"
+    //             alt="name logo"
+    //           />
+    //         </Navbar.Brand>
+    //       </Container>
+    //     </Navbar> 
+    )
+  }
+export default Header
