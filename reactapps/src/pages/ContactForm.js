e3234ac3c2ead98436a843f055e19a28868c0bca
chinapicke <chinapickering@hotmail.co.uk>
2023-03-07 10:11:48 +0700

    changes to contactform to increase form submission with netlify

--- reactapps/src/pages/ContactForm.js
+++ reactapps/src/pages/ContactForm.js
@@ -1,58 +1,68 @@
 import React from 'react'
-import Col from 'react-bootstrap/Col';
+// import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
+// import Row from 'react-bootstrap/Row';
+// import Button from 'react-bootstrap/Button';
 import '../styles/ContactForm.css';
-
+import { useState } from 'react';
 
 
 const ContactForm = () => {
-  const [formStatus, setFormStatus] = React.useState('Send')
-  const onSubmit = (e) => {
-    e.preventDefault()
-    setFormStatus('Submitting...')
-    const { name, email, message } = e.target.elements
-    let conForm = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    }
-    console.log(conForm)
-  }
+  // const [formStatus, setFormStatus] = React.useState('Send')
+  // const onSubmit = (e) => {
+  //   e.preventDefault()
+  //   setFormStatus('Submitting...')
+  //   const { name, email, message } = e.target.elements
+  //   let conForm = {
+  //     name: name.value,
+  //     email: email.value,
+  //     message: message.value,
+  //   }
+  //   console.log(conForm)
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+
   return (
-    <Form className='contactForm' name='contactForm' onSubmit={onSubmit} method="post" data-netlify="true">
-      <input type="hidden" name="form-name" value="contactForm" />
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>FirstName</Form.Label>
-          <Form.Control type="text" placeholder="Firstname" />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Surname</Form.Label>
-          <Form.Control type="text" placeholder="Surname" />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="example@hotmail.com" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Phone</Form.Label>
-        <Form.Control type="number"placeholder="Enter phone number" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Message</Form.Label>
-        <Form.Control type="text"placeholder="Enter your mesage" rows={5}/>
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit {formStatus}
-      </Button>
+    <Form>
+      <div className="form-group">
+        <label htmlFor="name">
+          Your Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">
+          Your Email
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">
+          Your message
+          <textarea
+            type="text"
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">Send</button>
     </Form>
   )
 }
